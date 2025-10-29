b89efffc42f74dc19250178c9a93e188991ebdd4
Jeff Putsch <jputsch@analog.com>
2025-10-29 15:21:19 -0700

    update to use "safe_symlink"

--- scripts/system.sh
+++ scripts/system.sh
@@ -11,13 +11,51 @@ if [ -f /usr/cadtools/bin/modules.dir/Shrc ]; then
     . /usr/cadtools/bin/modules.dir/Shrc ]
 fi
 
-
-
 # shellcheck source=../scripts/identify_platform.sh
 source "$(pwd)/scripts/identify_platform.sh"
 
 # shellcheck source=../scripts/util.sh
 source "$(pwd)/scripts/util.sh"
+
+
+safe_symlink() {
+    target="$1"
+    link_path="$2"
+    needs_link=0
+
+    # Check if link_path exists
+    if [ -e "$link_path" ] || [ -L "$link_path" ]; then
+        # Check if it's a symlink
+        if [ -L "$link_path" ]; then
+            # Get the target of the symlink
+            current_target=$(readlink "$link_path")
+
+            # Check if it already points to the correct location
+            if [ "$current_target" = "$target" ]; then
+                echo "Symlink already exists and points to the correct location"
+            else
+                # Symlink exists but points to wrong target - rename it
+                mv "$link_path" "$link_path.bak"
+                echo "Renamed existing symlink to $link_path.bak (was pointing to: $current_target)"
+                needs_link=1
+            fi
+        else
+            # It's a file or directory - rename it
+            mv "$link_path" "$link_path.bak"
+            echo "Renamed existing $link_path to $link_path.bak"
+            needs_link=1
+        fi
+    else
+        # Nothing exists at that path
+        needs_link=1
+    fi
+
+    # Create the symlink if needed
+    if [ "$needs_link" -eq 1 ]; then
+        ln -s "$target" "$link_path"
+        echo "Created symlink: $link_path -> $target"
+    fi
+}
 
 do_install_darwin() {
 	local install_dir
@@ -61,31 +99,31 @@ do_configure() {
     [[ -d $HOME/.paths.d ]] || mkdir -p $HOME/.paths.d
     if is_maxim_host; then
         for f in $(pwd)/system/paths-maxim.d/*; do
-            ln -sf $f ${HOME}/.paths.d/$(basename $f)
+            safe_symlink $f ${HOME}/.paths.d/$(basename $f)
         done
     fi
     for f in $(pwd)/system/paths.d/*; do
-        ln -sf $f ${HOME}/.paths.d/$(basename $f)
+        safe_symlink $f ${HOME}/.paths.d/$(basename $f)
     done
 
     info "[system][configure] Install \$HOME/etc dir"
     [ -d $HOME/etc ] || mkdir -p $HOME/etc
     if [ -d $(pwd)/system/home_etc.d ]; then
         for f in $(pwd)/system/home_etc.d/*; do
-            ln -sf $f ${HOME}/etc/$(basename $f)
+            safe_symlink $f ${HOME}/etc/$(basename $f)
         done
     fi
 
     info "[system][configure] Install \$HOME/lib dir"
     [ -d $HOME/lib ] || mkdir -p $HOME/lib
     for f in $( ls $(pwd)/system/home_lib.d 2>/dev/null ); do
-        ln -sf $f ${HOME}/lib/$(basename $f)
+        safe_symlink $f ${HOME}/lib/$(basename $f)
     done
 
     info "[system][configure] Install \$HOME/libexec dir"
     [ -d $HOME/libexec ] || mkdir -p $HOME/libexec
     for f in $( ls $(pwd)/system/home_libexec.d 2>/dev/null ); do
-        ln -sf $f ${HOME}/libexec/$(basename $f)
+        safe_symlink $f ${HOME}/libexec/$(basename $f)
     done
 
     info "[system][configure] Install \$HOME/log dir"
@@ -94,57 +132,35 @@ do_configure() {
         logdir=$(dirname "${HOME}/log/${f#*log.d/}")
         fname=$(basename $f)
         mkdir -p "${logdir}"
-        ln -sf $f "${logdir}/${fname}"
+        safe_symlink $f "${logdir}/${fname}"
     done
 
     info "[system][configure] Shell (csh, sh, zsh & bash) startup files"
-    info "[system][configure][[t]csh] .user.cshrc"
     if is_adi_host; then
-		ln -sf "$(pwd)/csh/cshrc.user" "${HOME}/.cshrc.user"
+        info "[system][configure][[t]csh] .user.cshrc"
+        safe_symlink "$(pwd)/csh/cshrc.user" "${HOME}/.cshrc.user"
 	fi
 
     info "[system][configure][terminfo] .terminfo"
-    # Check if $HOME/.terminfo exists
-    if [ -e "$HOME/.terminfo" ]; then
-        # Check if it's a symlink pointing to $HOME/.dotfiles/terminfo
-        if [ -L "$HOME/.terminfo" ]; then
-            # Get the target of the symlink (resolve it)
-            target=$(readlink "$HOME/.terminfo")
+    safe_symlink "$HOME/.dotfiles/terminfo" "$HOME/.terminfo"
 
-            # Check if it already points to the correct location
-            # We need to handle both absolute and relative paths
-            if [ "$target" = "$HOME/.dotfiles/terminfo" ] || \
-               [ "$target" = ".dotfiles/terminfo" ]; then
-               : already exists, do nothing
-            fi
-        fi
-
-        # If we reach here, either it's not a symlink or points elsewhere
-        # Rename it with .bak extension
-        mv "$HOME/.terminfo" "$HOME/.terminfo.bak"
-        echo "Renamed existing .terminfo to .terminfo.bak"
-    fi
-
-    # Create the symlink
-    ln -s "$HOME/.dotfiles/terminfo" "$HOME/.terminfo"
-
-    echo "Created symlink: $HOME/.terminfo -> $HOME/.dotfiles/terminfo"
     info "[system][configure][sh] .profile"
-    ln -sf "$(pwd)/sh/profile" "${HOME}/.profile"
+    safe_symlink "$(pwd)/sh/profile" "${HOME}/.profile"
+
     info "[system][configure][bash] .bash_profile"
-    ln -sf "$(pwd)/bash/bash_profile" "${HOME}/.bash_profile"
+    safe_symlink "$(pwd)/bash/bash_profile" "${HOME}/.bash_profile"
+
     info "[system][configure][bash] .bashrc"
-    ln -sf "$(pwd)/bash/bashrc" "${HOME}/.bashrc"
+    safe_symlink "$(pwd)/bash/bashrc" "${HOME}/.bashrc"
+
     info "[system][configure][zsh] .zprofile"
-    ln -sf "$(pwd)/zsh/zprofile" "${HOME}/.zprofile"
+    safe_symlink "$(pwd)/zsh/zprofile" "${HOME}/.zprofile"
+
     info "[system][configure][zsh] .zshrc"
-    ln -sf "$(pwd)/zsh/zshrc" "${HOME}/.zshrc"
+    safe_symlink "$(pwd)/zsh/zshrc" "${HOME}/.zshrc"
+
     info "[system][configure][common bash/zsh shell init] $HOME/.config/profile.d"
-    if [ -e ${HOME}/.config/profile.d ] && [ ! -h ${HOME}/.config/profile.d ]; then
-        rm -rf ${HOME}/.config/profile.d
-    elif [ ! -e ${HOME}/.config/profile.d ]; then
-        ln -sf "$(pwd)/config/profile.d" "${HOME}/.config/profile.d"
-    fi
+    safe_symlink "$(pwd)/config/profile.d" "${HOME}/.config/profile.d"
 
     info "[system][configure] Setup ADI modules"
     if type -t module >/dev/null 2>&1; then
@@ -153,8 +169,8 @@ do_configure() {
 
     # Install modules links:
     if type -t module >/dev/null 2>&1; then
-    	ln -sf "$(pwd)/modules/adi_modules" "${HOME}/.adi_modules"
-    	ln -sf "$(pwd)/modules/ownpath" "${HOME}/.ownpath"
+    	safe_symlink "$(pwd)/modules/adi_modules" "${HOME}/.adi_modules"
+    	safe_symlink "$(pwd)/modules/ownpath" "${HOME}/.ownpath"
     fi
 }
 
